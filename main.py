5ea412479313f159a006a54041160d73e093a429
Ilya Konoplev <incorrect1995@mail.ru>
2021-06-01 14:56:30 +0300

    default domain parsing fix

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ def get_posts(count, url):
     token = 'c8a9a3dbc8a9a3dbc8a9a3db82c8d1ac77cc8a9c8a9a3dba81d4f21975d685893981a54'
     version = 5.131
     posts = []
-    if url.startswith('club'):
+    if url.startswith(('club', 'public')):
         url = url.replace('club', '-')
+        url = url.replace('public', '-')
         response = requests.get('https://api.vk.com/method/wall.get',
                             params = {
                                 'access_token': token,
@@ -40,6 +41,6 @@ def parse_to_string(posts):
     return line
 
 
-posts = get_posts(100, 'club163745170')
+posts = get_posts(100, 'public158343732')
 s = parse_to_string(posts)
 print(s)
