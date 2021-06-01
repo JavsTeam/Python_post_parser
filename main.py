c7e05cad6110e1551a4b5883242bb1d687449418
Ilya Konoplev <incorrect1995@mail.ru>
2021-06-01 14:52:29 +0300

    default domain parsing fix

--- main.py
+++ main.py
@@ -5,15 +5,25 @@ def get_posts(count, url):
     token = 'c8a9a3dbc8a9a3dbc8a9a3db82c8d1ac77cc8a9c8a9a3dba81d4f21975d685893981a54'
     version = 5.131
     posts = []
-
-    response = requests.get('https://api.vk.com/method/wall.get',
-                        params = {
-                            'access_token': token,
-                            'v': version,
-                            'domain': url,
-                            'count': count,
-                        }
-                        )
+    if url.startswith('club'):
+        url = url.replace('club', '-')
+        response = requests.get('https://api.vk.com/method/wall.get',
+                            params = {
+                                'access_token': token,
+                                'v': version,
+                                'owner_id': url,
+                                'count': count,
+                            }
+                            )
+    else:
+        response = requests.get('https://api.vk.com/method/wall.get',
+                            params = {
+                                'access_token': token,
+                                'v': version,
+                                'domain': url,
+                                'count': count,
+                            }
+                            )
     data = response.json()['response']['items']
     posts.extend(data)
     return posts
@@ -30,6 +40,6 @@ def parse_to_string(posts):
     return line
 
 
-posts = get_posts(100, 'bugurt_thread')
+posts = get_posts(100, 'club163745170')
 s = parse_to_string(posts)
 print(s)
