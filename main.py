948f402e2f936f0f021441da36cb786ed1134bb6
Ilya Konoplev <incorrect1995@mail.ru>
2021-06-03 17:01:09 +0300

    added offset

--- main.py
+++ main.py
@@ -5,28 +5,32 @@ def get_posts(count, url):
     token = 'c8a9a3dbc8a9a3dbc8a9a3db82c8d1ac77cc8a9c8a9a3dba81d4f21975d685893981a54'
     version = 5.131
     posts = []
-    if url.startswith(('club', 'public')):
-        url = url.replace('club', '-')
-        url = url.replace('public', '-')
-        response = requests.get('https://api.vk.com/method/wall.get',
-                            params = {
-                                'access_token': token,
-                                'v': version,
-                                'owner_id': url,
-                                'count': count,
-                            }
-                            )
-    else:
-        response = requests.get('https://api.vk.com/method/wall.get',
-                            params = {
-                                'access_token': token,
-                                'v': version,
-                                'domain': url,
-                                'count': count,
-                            }
-                            )
-    data = response.json()['response']['items']
-    posts.extend(data)
+    offset = 0;
+
+    while offset < count:
+        if url.startswith(('club', 'public')):
+            url = url.replace('club', '-')
+            url = url.replace('public', '-')
+            response = requests.get('https://api.vk.com/method/wall.get',
+                                params = {
+                                    'access_token': token,
+                                    'v': version,
+                                    'owner_id': url,
+                                    'count': count,
+                                }
+                                )
+        else:
+            response = requests.get('https://api.vk.com/method/wall.get',
+                                params = {
+                                    'access_token': token,
+                                    'v': version,
+                                    'domain': url,
+                                    'count': count,
+                                }
+                                )
+        data = response.json()['response']['items']
+        posts.extend(data)
+        offset += 100;
     return posts
 
 
@@ -41,6 +45,6 @@ def parse_to_string(posts):
     return line
 
 
-posts = get_posts(100, 'public158343732')
+posts = get_posts(1000, 'bugurt_thread')
 s = parse_to_string(posts)
 print(s)
